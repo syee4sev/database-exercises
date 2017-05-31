8f99e6ebd67104e7e3d2f4ca7fade108e5c269df
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-31 10:49:38 -0500

    initial commit - sql lessons

--- tables_lesson.sql
+++ tables_lesson.sql
@@ -1,2 +1,35 @@
---tables lesson
+-- tables lesson
 
+use codeup_db;
+-- select * from mysql.user\G
+
+
+CREATE TABLE quotes (
+    id int unsigned not null auto_increment primary key,
+    author_first_name VARCHAR(50),
+    author_last_name  VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created_at datetime,
+    udpated_at datetime
+);
+
+CREATE table airlines(
+    id int unsigned not null auto_increment primary key,
+    airport_code char(3),
+    location varchar(255),
+    description text
+);
+
+
+create table people(
+    id int unsigned not null auto_increment primary key,
+    name varchar(255) not null,
+    age decimal(4,1) not null
+);
+
+create table users(
+    id int unsigned not null auto_increment primary key,
+    username varchar(255) not null,
+    password varchar(255) not null,
+    is_admin tinyint not null
+);
