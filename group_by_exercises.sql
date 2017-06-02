ff67d531eb18f9e67dcc485a770d1476a77a0518
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-06-02 10:22:03 -0500

    additinal group by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,9 +10,16 @@ GROUP BY first_name;
 
 select distinct title
 from titles
-order by title;
+group by title;
 
-select last_name
+select last_name, first_name
 from employees
 where (last_name like 'e%') and (last_name like '%e')
 group by last_name;
+
+select last_name
+from employees
+where (last_name like '%q%') and (last_name not like '%qu%')
+group by last_name;
+
+
